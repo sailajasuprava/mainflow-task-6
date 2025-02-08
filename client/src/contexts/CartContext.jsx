a010448031701f1f76f6cefe26dddb7c2de97a1b
sailajasuprava <sailaja.suprava@gmail.com>
2025-02-08 19:35:54 +0530

    authorization

---
+++ client/src/contexts/CartContext.jsx
@@ -0,0 +1,57 @@
+import { createContext, useContext, useEffect, useState } from "react";
+import toast from "react-hot-toast";
+import axios from "../lib/axios";
+
+const CartContext = createContext();
+
+function CartProvider({ children }) {
+  const [cart, setCart] = useState([]);
+
+  useEffect(() => {
+    fetchCartItems();
+  }, []);
+
+  async function fetchCartItems() {
+    try {
+      const res = await axios.get("/cart");
+      setCart(res.data.data);
+    } catch (error) {
+      toast.error(error.response.data.message);
+    }
+  }
+
+  async function updateQuantity(productId, quantity) {
+    try {
+      const res = await axios.patch("/cart", { productId, quantity });
+      setCart(res.data.data);
+    } catch (error) {
+      toast.error(error.response.data.message);
+    }
+  }
+
+  async function removeFromCart(productId) {
+    console.log(productId);
+
+    try {
+      const res = await axios.delete("/cart", { data: { productId } });
+      setCart(res.data.data);
+    } catch (error) {
+      toast.error(error.response.data.message);
+    }
+  }
+  return (
+    <CartContext.Provider
+      value={{ cart, setCart, fetchCartItems, updateQuantity, removeFromCart }}
+    >
+      {children}
+    </CartContext.Provider>
+  );
+}
+
+export function useCart() {
+  const context = useContext(CartContext);
+  if (!context) throw new Error("context used outside of provider");
+  return context;
+}
+
+export default CartProvider;
